8400bb17d8e4059e76e027a81ca5f8565342b714
Korol <92019385+eucalypt63@users.noreply.github.com>
2023-12-08 01:39:45 +0300

    Add files via upload

---
+++ TI1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace TI1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Key = new System.Windows.Forms.TextBox();
+            this.FirstFile = new System.Windows.Forms.Button();
+            this.FileSave = new System.Windows.Forms.Button();
+            this.TypeEncrypt = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.Action = new System.Windows.Forms.ComboBox();
+            this.StartEnc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IgNum = new System.Windows.Forms.CheckBox();
+            this.IgSym = new System.Windows.Forms.CheckBox();
+            this.IgLan = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Key
+            // 
+            this.Key.Location = new System.Drawing.Point(30, 284);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(139, 26);
+            this.Key.TabIndex = 0;
+            // 
+            // FirstFile
+            // 
+            this.FirstFile.Location = new System.Drawing.Point(39, 331);
+            this.FirstFile.Name = "FirstFile";
+            this.FirstFile.Size = new System.Drawing.Size(121, 29);
+            this.FirstFile.TabIndex = 1;
+            this.FirstFile.Text = "Choose File";
+            this.FirstFile.UseVisualStyleBackColor = true;
+            this.FirstFile.Click += new System.EventHandler(this.FirstFile_Click);
+            // 
+            // FileSave
+            // 
+            this.FileSave.Location = new System.Drawing.Point(192, 331);
+            this.FileSave.Name = "FileSave";
+            this.FileSave.Size = new System.Drawing.Size(121, 29);
+            this.FileSave.TabIndex = 2;
+            this.FileSave.Text = "Save into File";
+            this.FileSave.UseVisualStyleBackColor = true;
+            this.FileSave.Click += new System.EventHandler(this.FileSave_Click);
+            // 
+            // TypeEncrypt
+            // 
+            this.TypeEncrypt.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TypeEncrypt.FormattingEnabled = true;
+            this.TypeEncrypt.Items.AddRange(new object[] {
+            "Railway Fence",
+            "Vigenere"});
+            this.TypeEncrypt.Location = new System.Drawing.Point(30, 73);
+            this.TypeEncrypt.Name = "TypeEncrypt";
+            this.TypeEncrypt.Size = new System.Drawing.Size(177, 28);
+            this.TypeEncrypt.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // Action
+            // 
+            this.Action.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Action.FormattingEnabled = true;
+            this.Action.Items.AddRange(new object[] {
+            "Encrypt",
+            "Decipher"});
+            this.Action.Location = new System.Drawing.Point(257, 73);
+            this.Action.Name = "Action";
+            this.Action.Size = new System.Drawing.Size(109, 28);
+            this.Action.TabIndex = 4;
+            // 
+            // StartEnc
+            // 
+            this.StartEnc.Location = new System.Drawing.Point(339, 331);
+            this.StartEnc.Name = "StartEnc";
+            this.StartEnc.Size = new System.Drawing.Size(108, 29);
+            this.StartEnc.TabIndex = 5;
+            this.StartEnc.Text = "Start";
+            this.StartEnc.UseVisualStyleBackColor = true;
+            this.StartEnc.Click += new System.EventHandler(this.StartEnc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Choose Tipe of Encrypt:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Choose Action:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(409, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ignor List:";
+            // 
+            // IgNum
+            // 
+            this.IgNum.AutoSize = true;
+            this.IgNum.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.IgNum.Location = new System.Drawing.Point(3, 3);
+            this.IgNum.Name = "IgNum";
+            this.IgNum.Size = new System.Drawing.Size(99, 24);
+            this.IgNum.TabIndex = 11;
+            this.IgNum.Text = "Numbers";
+            this.IgNum.UseVisualStyleBackColor = true;
+            // 
+            // IgSym
+            // 
+            this.IgSym.AutoSize = true;
+            this.IgSym.Location = new System.Drawing.Point(3, 33);
+            this.IgSym.Name = "IgSym";
+            this.IgSym.Size = new System.Drawing.Size(95, 24);
+            this.IgSym.TabIndex = 12;
+            this.IgSym.Text = "Symbols";
+            this.IgSym.UseVisualStyleBackColor = true;
+            // 
+            // IgLan
+            // 
+            this.IgLan.AutoSize = true;
+            this.IgLan.Location = new System.Drawing.Point(3, 63);
+            this.IgLan.Name = "IgLan";
+            this.IgLan.Size = new System.Drawing.Size(162, 24);
+            this.IgLan.TabIndex = 13;
+            this.IgLan.Text = "Another language";
+            this.IgLan.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.IgNum);
+            this.panel1.Controls.Add(this.IgLan);
+            this.panel1.Controls.Add(this.IgSym);
+            this.panel1.Location = new System.Drawing.Point(413, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 94);
+            this.panel1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 394);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartEnc);
+            this.Controls.Add(this.Action);
+            this.Controls.Add(this.TypeEncrypt);
+            this.Controls.Add(this.FileSave);
+            this.Controls.Add(this.FirstFile);
+            this.Controls.Add(this.Key);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Encryption";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Key;
+        private System.Windows.Forms.Button FirstFile;
+        private System.Windows.Forms.Button FileSave;
+        private System.Windows.Forms.ComboBox TypeEncrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button StartEnc;
+        private System.Windows.Forms.ComboBox Action;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox IgNum;
+        private System.Windows.Forms.CheckBox IgSym;
+        private System.Windows.Forms.CheckBox IgLan;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TI1/Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace TI1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string FileWay = "";
+        public string FileSaveWay = "";
+
+        string CreateString (string str)
+        {
+            int n = str.Length;
+            char[] tempstr = new char[n];
+
+            char[] Num = {'0','1','2','3','4','5','6','7','8','9'};
+            char[] Sym = { '.', ',', '/', '!', '@', '#', '%', '?', '+', '-', '*', '&', '(', ')', '{','}', '[', ']', '|'};
+            char[] En = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            int i = 0;
+            int j = 0;
+            while (i < n)
+            {
+                if (IgNum.Checked && Num.Contains(str[i]))
+                {
+                    i++;
+                }
+                else if (IgSym.Checked && Sym.Contains(str[i]))
+                {
+                    i++;
+                }
+                else if (IgLan.Checked && En.Contains(str[i]))
+                {
+                    i++;
+                }
+                else if (str[i] == ' ')
+                {
+                    i++;
+                }
+                else
+                {
+                    tempstr[j] = str[i];
+                    j++;
+                    i++;
+                }
+            }
+            string s = new string(tempstr);//
+            return s;
+        }
+
+        string Iron(string str)
+        {
+
+            int m = 0;
+            if (int.TryParse(Key.Text, out m) && m > 1)
+            {
+                //Истинный текст
+                str = CreateString(str);
+                str = str.Trim();
+                int n = str.Length;
+
+                //Создание массива шифрования
+                char[,] mass = new char[m, n];
+                int i = 0;
+                int flag = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i, j] = str[j];
+                    if (flag == 1)
+                    {
+                        i++;
+                        if (i == m - 1)
+                        {
+                            flag = 0;
+                        }
+                    }
+                    else
+                    {
+                        i--;
+                        if (i == 0)
+                        {
+                            flag = 1;
+                        };
+                    }
+                }
+
+                //Чтение матрицы
+                char[] tstr = new char[n];
+                int tmpI = 0;
+                for (i = 0; i < m; i++)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        if (mass[i, j] != 0)
+                        {
+                            tstr[tmpI] = mass[i, j];
+                            tmpI++;
+                        }
+                    }
+                }
+                string s = new string(tstr);
+                return s;
+            }
+            else if (m == 1)
+            {
+                str = CreateString(str);
+                return str;
+            }
+            else
+            {
+                Key.Text = "Incorrect Key";
+                return str;
+            }
+        }
+        string DeIron(string str)
+        {
+            int m = 0;
+            if (int.TryParse(Key.Text, out m) && m > 1)
+            {
+                int n = str.Length;
+
+                //Создание массивов дешифрования
+                char[,] mass = new char[m, n];
+                char[,] demass = new char[m, n];
+                int i = 0;
+                int flag = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i, j] = str[j];
+                    demass[i, j] = str[j];
+                    if (flag == 1)
+                    {
+                        i++;
+                        if (i == m - 1)
+                        {
+                            flag = 0;
+                        }
+                    }
+                    else
+                    {
+                        i--;
+                        if (i == 0)
+                        {
+                            flag = 1;
+                        };
+                    }
+                }
+
+                //Востановление массива шифрования
+                int tmpI = 0;
+                int tmpJ = 0;
+                flag = 1;
+                for (i = 0; i < m; i++)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        if (demass[i, j] != 0)
+                        {
+                            demass[i, j] = mass[tmpI, tmpJ];
+                            
+                            if (flag == 1)
+                            {
+                                tmpI++;
+                                if (tmpI == m - 1)
+                                {
+                                    flag = 0;
+                                }
+                            }
+                            else
+                            {
+                                tmpI--;
+                                if (tmpI == 0)
+                                {
+                                    flag = 1;
+                                };
+                            }
+                            tmpJ++;
+                            
+                        }
+                    }
+                }
+
+                //Чтение массива
+                char[] tstr = new char[n];
+                i = 0;
+                flag = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    tstr[j] = demass[i, j];
+                    if (flag == 1)
+                    {
+                        i++;
+                        if (i == m - 1)
+                        {
+                            flag = 0;
+                        }
+                    }
+                    else
+                    {
+                        i--;
+                        if (i == 0)
+                        {
+                            flag = 1;
+                        }
+                    }
+                }
+                string s = new string(tstr);
+                return s;
+                //
+            }
+            else if (m == 1)
+            {
+                return str;
+            }
+            else 
+            {
+                Key.Text = "Incorrect Key";
+                return str;
+            }
+        }
+
+        int symbls(char[] Symb, int h, char ch)
+        {
+            int i = 0;
+            for (; i < 33 && Symb[i] != ch; i++)
+            { }
+
+            //26
+            if (i < 33)
+            {
+                i = (i + h) % 33;
+
+                return i;
+            }
+            else return -1;
+        }
+        string Vigen(string str, int Flag)
+        {
+            //char[] Symb = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }; 26
+            char[] Symb = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' };
+            if (Flag == 1) //Если шифруем
+            {
+                str = CreateString(str);
+                str = str.Trim();
+            }
+            int n = str.Length;
+
+            //Создание и проверка ключа
+            string TKey = Key.Text;
+            TKey = TKey.Trim();
+            TKey = TKey.ToLower();
+
+            int k = 0;
+
+            for (;k < TKey.Length && symbls(Symb, 0, TKey[k]) != -1; k++ )
+            {  }
+
+            if (k == TKey.Length)
+            {
+                // Создание истенного ключа
+                char[] VKey = new char[n];
+                int tmpI = 0;
+                int h = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (symbls(Symb, 0, str[i]) != -1)
+                    {
+                        char tmpCh = Symb[symbls(Symb, h, TKey[tmpI])];
+                        VKey[i] = tmpCh;
+
+                        tmpI++;
+                        if (tmpI == TKey.Length)
+                        {
+                            tmpI = 0;
+                            h++;
+                        }
+                    }
+                }
+                //Проверка шифра и дешифра
+                int Sig = 1;
+                if (Flag == 0)
+                    Sig = -1;
+
+                //Шифр/Дешифр текста
+                char[] tmpStr = new char[n];
+                for (int i = 0; i < n; i++)
+                {
+                    if (symbls(Symb, 0, str[i]) != -1)
+                    {
+                        int tmp = (symbls(Symb, 0, str[i]) + (symbls(Symb, 0, VKey[i])) * Sig + 33) % 33;
+                        tmpStr[i] = Symb[tmp];
+                    }
+                    else tmpStr[i] = str[i];
+                }
+                string s = new string(tmpStr);//
+                return s;
+            }
+            else
+            {
+                Key.Text = "Incorrect Key";
+                return str;
+            }
+        }
+
+        private void FirstFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileWay = openFileDialog1.FileName;
+            }
+        }
+        private void FileSave_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                FileSaveWay = openFileDialog2.FileName;
+            }
+        }
+
+        private void StartEnc_Click(object sender, EventArgs e)
+        {
+            if (FileWay != "" && FileSaveWay != "" && TypeEncrypt.Text != "" && Action.Text != "" && Key.Text != "")
+            {
+                if (TypeEncrypt.Text == "Railway Fence")
+                {
+                    string Temp = "";
+                    if (Action.Text == "Encrypt")
+                    {
+                       
+                        StreamReader reader = new StreamReader(FileWay);
+                        StreamWriter sw = new StreamWriter(FileSaveWay, false);
+
+                        for (string str; (str = reader.ReadLine()) != null; )
+                        {
+                            str = str.ToLower();
+                            Temp += str;      
+                        }
+                        Temp = Iron(Temp);
+                        sw.WriteLine(Temp);
+
+                        reader.Close();
+                        sw.Close();
+                    }
+                    else 
+                    {
+                        StreamReader reader = new StreamReader(FileWay);
+                        StreamWriter sw = new StreamWriter(FileSaveWay, false);
+
+                        for (string str; (str = reader.ReadLine()) != null;)
+                        {
+                            str = str.ToLower();
+                            Temp += str;
+                        }
+                        Temp = DeIron(Temp);
+                        sw.WriteLine(Temp);
+
+                        reader.Close();
+                        sw.Close();
+                    }
+                }
+                else if (TypeEncrypt.Text == "Vigenere")
+                {
+                    string Temp = "";
+                    if (Action.Text == "Encrypt")
+                    {
+                        StreamReader reader = new StreamReader(FileWay);
+                        StreamWriter sw = new StreamWriter(FileSaveWay, false);
+
+                        for (string str; (str = reader.ReadLine()) != null;)
+                        {
+                            str = str.ToLower();
+                            Temp += str;
+                        }
+                        Temp = Vigen(Temp, 1);
+                        sw.WriteLine(Temp);
+
+                        reader.Close();
+                        sw.Close();
+                    }
+                    else 
+                    {
+                        StreamReader reader = new StreamReader(FileWay);
+                        StreamWriter sw = new StreamWriter(FileSaveWay, false);
+
+                        for (string str; (str = reader.ReadLine()) != null;)
+                        {
+                            str = str.ToLower();
+                            Temp += str; 
+                        }
+                        Temp = Vigen(Temp, 0);
+                        sw.WriteLine(Temp);
+
+                        reader.Close();
+                        sw.Close();
+                    }
+                }
+            }
+        }
+    }
+}
