601d6d554aff05653bae2279e488a130bf75f227
Korol <92019385+eucalypt63@users.noreply.github.com>
2023-12-08 01:42:22 +0300

    Add files via upload

---
+++ TI3/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace TI3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.TypeED = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(31, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(158, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(31, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 26);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 75;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(158, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter k:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter p:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Select g:";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(31, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate g";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 171);
+            this.panel1.TabIndex = 9;
+            // 
+            // TypeED
+            // 
+            this.TypeED.DropDownHeight = 75;
+            this.TypeED.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TypeED.FormattingEnabled = true;
+            this.TypeED.IntegralHeight = false;
+            this.TypeED.Items.AddRange(new object[] {
+            "Encryption",
+            "Decryption"});
+            this.TypeED.Location = new System.Drawing.Point(307, 40);
+            this.TypeED.Name = "TypeED";
+            this.TypeED.Size = new System.Drawing.Size(121, 28);
+            this.TypeED.TabIndex = 10;
+            this.TypeED.SelectedIndexChanged += new System.EventHandler(this.TypeED_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(303, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Select Action:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Open File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(158, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save to File...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(31, 257);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(121, 35);
+            this.StartButton.TabIndex = 14;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(586, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(109, 391);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(337, 405);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 26);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(333, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "C:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(471, 40);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(109, 393);
+            this.richTextBox2.TabIndex = 18;
+            this.richTextBox2.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(467, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Encrypt Text:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(582, 17);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 20);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "a and b:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 454);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TypeED);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox TypeED;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ TI3/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TI3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool FlagG = true;
+        string OpenFileName = "";
+        string SaveFileName = "";
+
+        //Нахождение g
+        private int ModPow(int @base, int exponent, int modulus)
+        {
+            int result = 1;
+            while (exponent > 0)
+            {
+                if ((exponent & 1) == 1)
+                    result = (result * @base) % modulus;
+                
+                @base = (@base * @base) % modulus;
+                exponent >>= 1;
+            }
+            return result;
+        }
+
+        private bool IsPrimitiveRoot(int a, int p)
+        {
+            HashSet<int> powers = new HashSet<int>();
+            for (int i = 1; i <= p - 1; i++)
+                powers.Add(ModPow(a, i, p));
+            
+            return powers.Count == p - 1;
+        }
+
+        //Проверка на простоту
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+                
+            return true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;     
+            }
+            else
+            {
+                comboBox1.Items.Clear();
+                comboBox1.Enabled = false;
+                FlagG = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox3.Text, out int p);
+            if (IsPrime(p))
+            {
+                comboBox1.Enabled = true;
+                FlagG = true;
+                int i = 0;
+                for (int a = 1; a < p; a++)
+                    if (IsPrimitiveRoot(a, p))
+                    {
+                        comboBox1.Items.Add(a);
+                        i++;
+                    }
+                textBox4.Text = i + "";
+            }
+            else
+            {
+                MessageBox.Show("P are not Prime!");
+            }
+        }
+
+        private void TypeED_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (TypeED.Text == "Encryption")
+            {
+                textBox3.Enabled = true;
+                textBox2.Enabled = true;
+                textBox1.Enabled = true;
+                button1.Enabled = true;
+
+                if (FlagG)
+                    comboBox1.Enabled = true;
+            }
+            else if (TypeED.Text == "Decryption")
+            {
+                textBox3.Enabled = true;
+                textBox1.Enabled = true;
+
+                textBox2.Enabled = false;
+                button1.Enabled = false;
+                comboBox1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                OpenFileName = openFileDialog1.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                SaveFileName = saveFileDialog1.FileName;
+        }
+
+        //Эвклид k p-1
+        public static bool AreCoprime(int a, int b)
+        {
+            int x, y;
+            int gcd = ExtendedEuclidean(a, b, out x, out y);
+            if (gcd != 1) return false;
+            return true;
+        }
+
+        public static int ExtendedEuclidean(int a, int b, out int x, out int y)
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+            int x1, y1;
+            int gcd = ExtendedEuclidean(b % a, a, out x1, out y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return gcd;
+        }
+
+        public static BigInteger Power(int x, int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return x;
+
+            BigInteger t = Power(x, n / 2);
+            if (n % 2 == 0) return t * t;
+            else return t * t * x;
+        }
+        
+        private int GetBytese (int p)
+        {
+            byte[] intBytes = BitConverter.GetBytes(p);
+            int i = 3;
+            for (; i > 0; i--)
+                if (intBytes[i] != 0)
+                    break;
+
+            return i;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (TypeED.Text == "Encryption")
+            {
+                richTextBox1.Clear();
+                richTextBox2.Clear();
+                int.TryParse(textBox1.Text, out int x);
+                int.TryParse(textBox2.Text, out int k);
+                int.TryParse(textBox3.Text, out int p);
+                int.TryParse(comboBox1.Text, out int g);
+
+                int sizeB = GetBytese(p);
+
+                BigInteger.DivRem(Power(g, x), p, out BigInteger temp);
+                int y = (int)temp;
+                textBox4.Text = y.ToString();
+                if (AreCoprime(p - 1, k) && k > 1 && x > 1 && k < p - 1 && x < p - 1 && p > 256)
+                {
+                    BinaryReader f2 = new BinaryReader(File.Open(OpenFileName, FileMode.Open));
+                    BinaryWriter f1 = new BinaryWriter(File.Open(SaveFileName, FileMode.OpenOrCreate));
+
+                    byte[] bytes = f2.ReadBytes((int)f2.BaseStream.Length);
+
+                    int b = 0;
+                    BigInteger.DivRem(Power(g, k), p, out temp);
+                    int a = (int)temp;
+                    int s = 0;
+                    foreach (byte by in bytes)
+                    {
+                        BigInteger.DivRem(Power(y, k) * by, p, out temp);
+                        b = (int)temp;
+
+                        if (s < 40)
+                        {
+                            richTextBox1.AppendText(a + " " + b + "\r\n");
+                            richTextBox2.AppendText(by + "\r\n");
+                            s++;
+                        }
+
+                        byte[] intBytes = BitConverter.GetBytes(a);
+
+                        for (int i = 0; i <= sizeB; i++)
+                            f1.Write(intBytes[i]);
+
+                        intBytes = BitConverter.GetBytes(b);
+                        for (int i = 0; i <= sizeB; i++)
+                            f1.Write(intBytes[i]);
+                    }
+                    f1.Close();
+                    f2.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error Input");
+                }
+            }
+            else if (TypeED.Text == "Decryption")
+            {
+                int.TryParse(textBox1.Text, out int x);
+                int.TryParse(textBox3.Text, out int p);
+
+                if (IsPrime(p))
+                {
+                    int sizeB = GetBytese(p);
+
+                    BinaryReader f2 = new BinaryReader(File.Open(OpenFileName, FileMode.Open));
+                    BinaryWriter f1 = new BinaryWriter(File.Open(SaveFileName, FileMode.OpenOrCreate));
+
+                    byte[] bytes = f2.ReadBytes((int)f2.BaseStream.Length);
+
+                    int A = 0;
+                    int B = 0;
+
+                    byte[] intBytes = new byte[] { 0, 0, 0, 0 };
+
+                    int tempI = 0;
+                    BigInteger temp;
+                    for (; tempI < bytes.Length;)
+                    {
+                        for (int k = 0; k < sizeB + 1; k++)
+                        {
+                            intBytes[k] = bytes[tempI];
+                            tempI++;
+                        }
+                        A = BitConverter.ToInt32(intBytes, 0);
+
+                        for (int k = 0; k < sizeB + 1; k++)
+                        {
+                            intBytes[k] = bytes[tempI];
+                            tempI++;
+                        }
+                        B = BitConverter.ToInt32(intBytes, 0);
+
+                        BigInteger.DivRem(B * Power(A, p - 1 - x), p, out temp);
+
+                        byte b = (byte)temp;
+                        
+                        f1.Write(b);
+                    }
+
+                    f1.Close();
+                    f2.Close();
+                }
+                else
+                {
+                    MessageBox.Show("P are not Prime!");
+                }
+            }
+        }
+    }
+}
